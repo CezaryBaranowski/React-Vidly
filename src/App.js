57e778bb0a2c086177c6a4c85daaf1d01ce6dbd9
CezaryBaranowski <czarekbaranowski2ice@gmail.com>
2020-07-27 18:42:16 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
 import Movies from "./components/Movies";
-import {Route} from "react-router-dom";
+import NavBar from "./components/navBar";
+import NotFound from "./components/notFound";
+import Rentals from "./components/Rentals";
+import Customers from "./components/customers";
+import MovieForm from "./components/MovieForm";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
